60d2da5b35e66a8e42121087c5e61342911a452c
kamal <skamalkumar@gmail.com>
2022-08-13 00:40:46 +0530

    first commit

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from todo import views
 from django.views.static import serve
-from django.config.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,6 +36,9 @@ urlpatterns = [
     path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
     path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
+
+if settings.DEBUG:
+  urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
