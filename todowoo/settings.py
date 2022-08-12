16a47968080d5d1be6b424d8738836f743e89bfe
kamal <skamalkumar@gmail.com>
2022-08-12 20:34:00 +0530

    first commit

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -122,3 +122,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_URL = '/login'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
