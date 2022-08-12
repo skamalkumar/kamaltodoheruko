e0aa0441a585c04eeca89032e605557fa3caca18
kamal <skamalkumar@gmail.com>
2022-08-12 20:37:46 +0530

    first commit

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -123,11 +123,11 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = '/login'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
